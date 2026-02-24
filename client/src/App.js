fbd52d6a1fcea6d2dc2c0d3303ed7f3a3b26f53f
kevinnail <fumalicious@gmail.com>
2026-02-24 12:29:42 -0800

    add auction routes and components for auction management, checkout route

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,11 @@ import AboutProject from './components/About/AboutProject.js';
 import MessagingContainer from './components/Messaging/MessagingContainer.js';
 import { useNotificationStore } from './stores/useNotificationStore.js';
 import PasswordChange from './components/Auth/AuthForms.js/PasswordChange.js';
+import AuctionList from './components/Auctions/AuctionList.js';
+import AuctionArchive from './components/Auctions/AuctionArchive.js';
+import AuctionDetail from './components/Auctions/AuctionDetail.js';
+import AuctionForm from './components/Auctions/AuctionForm.js';
+import Checkout from './components/Checkout/Checkout.js';
 
 //? this is referenced in ResponsiveAppBar for replacing title to display which development local/ deploy is in the browser tab
 window.REACT_APP_BASE_URL = window.location.origin;
@@ -159,9 +164,15 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/about-project" element={<AboutProject />} />
             <Route path="/stripe-return" element={<StripeReturn />} />
+            <Route path="/auctions" element={<AuctionList />} />
+            <Route path="/auctions/archive" element={<AuctionArchive />} />
+            <Route path="/auctions/:id" element={<AuctionDetail />} />
             <Route path="/dashboard" element={<DashboardTabs />} />
             <Route path="/dashboard/new" element={<NewPost />} />
             <Route path="/dashboard/edit/:id" element={<EditPost />} />
+            <Route path="/dashboard/auctions/new" element={<AuctionForm />} />
+            <Route path="/dashboard/auctions/:id/edit" element={<AuctionForm />} />
+            <Route path="/checkout" element={<Checkout />} />
             <Route path="/profile/:sub" element={<Profile />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/messages" element={<MessagingContainer />} />
