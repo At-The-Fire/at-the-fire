af7e26e711c42976930be900ab84444c46c1413b
kevinnail <fumalicious@gmail.com>
2026-02-25 13:50:13 -0800

    fix: update user identifier references to use req.userAWSSub in auction notifications controller

--- server/lib/controllers/auctionNotifications.js
+++ server/lib/controllers/auctionNotifications.js
@@ -6,7 +6,7 @@ module.exports = Router()
   // Get unread auction notifications for the current user
   .get('/', authenticateAWS, async (req, res, next) => {
     try {
-      const userSub = req.user.sub;
+      const userSub = req.userAWSSub;
       const notifications = await AuctionNotification.getUnreadByUserSub(userSub);
       res.json(notifications);
     } catch (e) {
@@ -17,7 +17,7 @@ module.exports = Router()
   // Mark all auction notifications as read for the current user
   .patch('/mark-read', authenticateAWS, async (req, res, next) => {
     try {
-      const userSub = req.user.sub;
+      const userSub = req.userAWSSub;
       await AuctionNotification.markAsRead(userSub);
       res.json({ message: 'All auction notifications marked as read' });
     } catch (e) {
