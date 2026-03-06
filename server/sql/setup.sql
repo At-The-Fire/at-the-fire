d8337dd2a6d999d5cf7e7f780d2113a022d0488b
kevinnail <fumalicious@gmail.com>
2026-03-06 09:18:23 -0800

    fix: standardize UUID format in SQL inserts for user and customer data

--- server/sql/setup.sql
+++ server/sql/setup.sql
@@ -36,7 +36,7 @@ id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 sub VARCHAR(255) UNIQUE NOT NULL,
 email VARCHAR(255) UNIQUE NOT NULL,
 customer_id VARCHAR(255) UNIQUE,
-created_at TIMESTAMP DEFAULT NOW(),  
+created_at TIMESTAMP DEFAULT NOW(),
 image_url VARCHAR(255),
 public_id VARCHAR(255),
 first_name VARCHAR(255),
@@ -44,7 +44,7 @@ last_name VARCHAR(255),
 bio VARCHAR(255),
 email_hash VARCHAR(255) UNIQUE NOT NULL,
 social_media_links JSON
-);  
+);
 
 CREATE TABLE stripe_customers (
 aws_sub VARCHAR(255) UNIQUE NOT NULL,
@@ -59,7 +59,7 @@ logo_image_url VARCHAR(255),
 logo_public_id VARCHAR(255),
 confirmed BOOLEAN DEFAULT FALSE,
 email_hash VARCHAR(255) UNIQUE ,
-FOREIGN KEY (aws_sub) REFERENCES cognito_users(sub) ON DELETE CASCADE 
+FOREIGN KEY (aws_sub) REFERENCES cognito_users(sub) ON DELETE CASCADE
 );
 
 CREATE TABLE subscriptions (
@@ -85,7 +85,7 @@ CREATE TABLE invoices (
   amount_due DECIMAL(9,2),
   amount_paid DECIMAL(9,2),
   created_at TIMESTAMP DEFAULT NOW(),
-  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id) ON DELETE CASCADE 
+  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE cancellation_data (
@@ -129,7 +129,7 @@ CREATE TABLE posts_imgs (
   image_url VARCHAR(255),
   public_id VARCHAR(255),
   resource_type VARCHAR(255),
-  FOREIGN KEY (post_id) REFERENCES gallery_posts(id) ON DELETE CASCADE 
+  FOREIGN KEY (post_id) REFERENCES gallery_posts(id) ON DELETE CASCADE
 );
 
 CREATE TABLE quota_tracking (
@@ -165,9 +165,9 @@ CREATE TABLE quota_goals (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   monthly_quota BIGINT,
-  work_days BIGINT, 
+  work_days BIGINT,
   customer_id VARCHAR(255) UNIQUE NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id)  ON DELETE CASCADE 
+  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id)  ON DELETE CASCADE
 );
 
 CREATE TABLE inventory_snapshot (
@@ -176,7 +176,7 @@ CREATE TABLE inventory_snapshot (
   category_count JSONB,
   price_count JSONB,
   customer_id VARCHAR(255) NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id) ON DELETE CASCADE 
+  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE orders (
@@ -199,11 +199,11 @@ CREATE TABLE image_uploads(
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   image_count INTEGER NOT NULL CHECK (image_count > 0),
   customer_id VARCHAR(255) NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id)  ON DELETE CASCADE 
+  FOREIGN KEY (customer_id) REFERENCES stripe_customers(customer_id)  ON DELETE CASCADE
 );
 
 -- Add index for the queries you'll be running in image_uploads table
-CREATE INDEX idx_image_uploads_customer_time 
+CREATE INDEX idx_image_uploads_customer_time
 ON image_uploads (customer_id, created_at);
 
 CREATE TABLE webhook_events (
@@ -342,38 +342,38 @@ CREATE TABLE purchases (
 
 --  adding users for testing --
 -- User with no profile data
-INSERT INTO "cognito_users" 
-("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash") 
-VALUES 
-(NULL, NOW(), NULL, 'noProfile@example.com', NULL, NULL, NULL, NULL, 'sub_noProfile', 'dummy_hash_5');
+INSERT INTO "cognito_users"
+("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash")
+VALUES
+(NULL, NOW(), NULL, 'noProfile@example.com', NULL, NULL, NULL, NULL, '123e4567-e89b-42d3-a456-426614174101', 'dummy_hash_5');
 
 -- User with profile data but no associated customer data
-INSERT INTO "cognito_users"  ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash") 
-VALUES  
-('Bio for profile user', NOW(), NULL, 'withProfile@example.com', 'First', 'image_url_path', 'Last', 'publicID_profile', 'sub_withProfile', 'dummy_hash_6');
+INSERT INTO "cognito_users"  ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash")
+VALUES
+('Bio for profile user', NOW(), NULL, 'withProfile@example.com', 'First', 'image_url_path', 'Last', 'publicID_profile', '123e4567-e89b-42d3-a456-426614174102', 'dummy_hash_6');
 
 -- User with a customer ID but no profile data entered in stripe_customers
-INSERT INTO "cognito_users" ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash") 
-VALUES (NULL, NOW(), 'stripe-customer-id_noProfile', 'customerNoProfile@example.com', NULL, NULL, NULL, NULL, 'sub_customerNoProfile', 'dummy_hash_7');
+INSERT INTO "cognito_users" ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash")
+VALUES (NULL, NOW(), 'stripe-customer-id_noProfile', 'customerNoProfile@example.com', NULL, NULL, NULL, NULL, '123e4567-e89b-42d3-a456-426614174103', 'dummy_hash_7');
 
-INSERT INTO "stripe_customers" 
-("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash") 
-VALUES 
-('sub_customerNoProfile', 'stripe-customer-id_noProfile', NULL, 'customerNoProfile@example.com', NULL, NULL, NULL, NULL, NULL, true, 'dummy_hash_8');
+INSERT INTO "stripe_customers"
+("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash")
+VALUES
+('123e4567-e89b-42d3-a456-426614174103', 'stripe-customer-id_noProfile', NULL, 'customerNoProfile@example.com', NULL, NULL, NULL, NULL, NULL, true, 'dummy_hash_8');
 
 -- Customer with profile data
-INSERT INTO "cognito_users"  ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash") 
-VALUES 
-('Bio for full customer', NOW(), 'stripe-customer-id_full', 'fullCustomer@example.com', 'CustomerFirst', 'image_url_path', 'CustomerLast', 'publicID_fullCustomer', 'sub_fullCustomer', 'dummy_hash_9');
+INSERT INTO "cognito_users"  ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash")
+VALUES
+('Bio for full customer', NOW(), 'stripe-customer-id_full', 'fullCustomer@example.com', 'CustomerFirst', 'image_url_path', 'CustomerLast', 'publicID_fullCustomer', '123e4567-e89b-42d3-a456-426614174104', 'dummy_hash_9');
 
 -- Test Customer (2) with profile data
-INSERT INTO "cognito_users"  ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash") 
-VALUES 
+INSERT INTO "cognito_users"  ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash")
+VALUES
 ('Bio for full customer 2', NOW(), 'cus_OVLKmXa6lrzktm', 'kevinnail@hotmail.com', 'CustomerTestFirst', 'image_url_path', 'CustomerTestLast', 'publicID_fullTestCustomer', '0861e380-30a1-70f8-82fd-0dc85cc975aa', 'dummy_hash_15');
 
 -- Test Customer (2) with customer data
-INSERT INTO "stripe_customers" 
-("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash") 
+INSERT INTO "stripe_customers"
+("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash")
 VALUES   ('0861e380-30a1-70f8-82fd-0dc85cc975aa', 'cus_OVLKmXa6lrzktm', 'Test Customer 2', 'kevinnail@hotmail.com', 'logo_image_url_path2', 'publicID_logo2', 'FullName2', '555-3333', 'http://website.com', true, 'dummy_hash_15');
 
  -- dTest Customer (2)  data insertions for testing
@@ -385,9 +385,9 @@ INSERT INTO orders (customer_id, order_number, date, is_fulfilled, client_name, 
 
 
 
-INSERT INTO "stripe_customers" 
-("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash") 
-VALUES   ('sub_fullCustomer', 'stripe-customer-id_full', 'Display Name', 'fullCustomer@example.com', 'logo_image_url_path', 'publicID_logo', 'FullName', '555-1234', 'http://website.com', true, 'dummy_hash_10');
+INSERT INTO "stripe_customers"
+("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash")
+VALUES   ('123e4567-e89b-42d3-a456-426614174104', 'stripe-customer-id_full', 'Display Name', 'fullCustomer@example.com', 'logo_image_url_path', 'publicID_logo', 'FullName', '555-1234', 'http://website.com', true, 'dummy_hash_10');
 
 INSERT INTO "quota_goals"
 ("monthly_quota","work_days","customer_id")
@@ -400,34 +400,34 @@ VALUES ('auction', 1731744000000, 'test-title','test-description','test-category
 
 -- Add posts for testing
 -- Posts for the user with a customer ID but no profile data entered in stripe_customers
-INSERT INTO "gallery_posts" 
+INSERT INTO "gallery_posts"
 ("title", "description", "image_url", "category", "price", "customer_id", "public_id", "num_imgs","sold")
-VALUES 
+VALUES
 ('SampleTitle1', 'SampleDescription1', 'sample_image_url_path_1', 'SampleCategory1', 'SamplePrice1', 'stripe-customer-id_noProfile', 'publicID_post_1', 1,false),
 ('SampleTitle2', 'SampleDescription2', 'sample_image_url_path_2', 'SampleCategory2', 'SamplePrice2', 'stripe-customer-id_noProfile', 'publicID_post_2', 2,false);
 
 -- Posts for the customer with profile data
-INSERT INTO "gallery_posts" 
+INSERT INTO "gallery_posts"
 ("title", "description", "image_url", "category", "price", "customer_id", "public_id", "num_imgs","sold")
-VALUES 
+VALUES
 ('SampleTitle3', 'SampleDescription3', 'sample_image_url_path_3', 'SampleCategory3', 'SamplePrice3', 'stripe-customer-id_full', 'publicID_post_3', 1,false),
 ('SampleTitle4', 'SampleDescription4', 'sample_image_url_path_4', 'SampleCategory4', 'SamplePrice4', 'stripe-customer-id_full', 'publicID_post_4', 2,false);
 
 -- Add partial profile data for testing
-INSERT INTO "cognito_users" 
+INSERT INTO "cognito_users"
 ("bio", "created_at", "customer_id", "email", "first_name", "image_url", "last_name", "public_id", "sub", "email_hash")
 VALUES
-(NULL, NOW(), 'stripe-customer-id_partialProfile', 'partialProfile@example.com', '', '', '', '', 'sub_partialProfile', 'dummy_hash_11');
+(NULL, NOW(), 'stripe-customer-id_partialProfile', 'partialProfile@example.com', '', '', '', '', '123e4567-e89b-42d3-a456-426614174105', 'dummy_hash_11');
 
-INSERT INTO "stripe_customers" 
+INSERT INTO "stripe_customers"
 ("aws_sub", "customer_id", "display_name", "email", "logo_image_url", "logo_public_id", "name", "phone", "website_url", "confirmed", "email_hash")
-VALUES 
-('sub_partialProfile', 'stripe-customer-id_partialProfile', 'Partial Profile Display Name', 'partialProfile@example.com' , ' ', ' ', 'Steve ', '555-1234 ', ' ', true, 'dummy_hash_12');
+VALUES
+('123e4567-e89b-42d3-a456-426614174105', 'stripe-customer-id_partialProfile', 'Partial Profile Display Name', 'partialProfile@example.com' , ' ', ' ', 'Steve ', '555-1234 ', ' ', true, 'dummy_hash_12');
 
 -- Add Inventory Snapshot data for testing
-INSERT INTO "inventory_snapshot" 
-("category_count",  "customer_id",  "price_count") 
-VALUES 
+INSERT INTO "inventory_snapshot"
+("category_count",  "customer_id",  "price_count")
+VALUES
 ('{"Beads":2,"Marbles":3,"Bubblers":1,"Recyclers":1,"Dry Pieces":2}',  'stripe-customer-id_full', '{"Beads":916,"Marbles":950,"Bubblers":500,"Recyclers":2000,"Dry Pieces":1400}');
 
 INSERT INTO "invoices"
@@ -444,27 +444,27 @@ VALUES
 INSERT INTO "followers"
 ("follower_id","followed_id","created_at")
 VALUES
-( 'sub_withProfile','sub_fullCustomer','2025-02-01 08:49:09.56484');
+( '123e4567-e89b-42d3-a456-426614174102','123e4567-e89b-42d3-a456-426614174104','2025-02-01 08:49:09.56484');
 
---  conversation 
+--  conversation
 INSERT INTO "conversations"
-DEFAULT VALUES 
+DEFAULT VALUES
 RETURNING id;
 
 INSERT INTO "conversation_participants"
 ("conversation_id", "user_sub")
-VALUES 
-(1, 'sub_noProfile'),
-(1, 'sub_fullCustomer');
+VALUES
+(1, '123e4567-e89b-42d3-a456-426614174101'),
+(1, '123e4567-e89b-42d3-a456-426614174104');
 
 INSERT INTO "conversation_visibility"
 ("conversation_id", "user_sub", "is_visible")
-VALUES 
-(1, 'sub_noProfile', true),
-(1, 'sub_fullCustomer', true);
+VALUES
+(1, '123e4567-e89b-42d3-a456-426614174101', true),
+(1, '123e4567-e89b-42d3-a456-426614174104', true);
 
-INSERT INTO "messages" 
+INSERT INTO "messages"
 ("conversation_id", "sender_sub", "content", "is_read")
-VALUES 
-(1, 'sub_noProfile', 'First test message!', false),
-(1, 'sub_fullCustomer', 'Second test message!', false);
+VALUES
+(1, '123e4567-e89b-42d3-a456-426614174101', 'First test message!', false),
+(1, '123e4567-e89b-42d3-a456-426614174104', 'Second test message!', false);
