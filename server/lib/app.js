bc71260819d1b373faab68033189cf5ee709c76f
kevinnail <fumalicious@gmail.com>
2026-03-13 21:42:04 -0700

    feat: add Cloudflare domain regex to CORS configuration

--- server/lib/app.js
+++ server/lib/app.js
@@ -28,6 +28,7 @@ app.use(
       'https://www.atthefire.com',
       'https://at-the-fire.herokuapp.com', //! production server
       'https://at-the-fire-dev-68560297982b.herokuapp.com', //^ development server
+      '  /\.trycloudflare\.com$/',
     ],
     credentials: true,
   }),
